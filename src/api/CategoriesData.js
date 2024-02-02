d0bcf0ee6982f5f9d873013ce9fa515f2d98f6d7
mgdeveloper1406 <mg.developer@yahoo.com>
2024-02-02 13:27:26 -0300

    edicion

--- src/api/CategoriesData.js
+++ src/api/CategoriesData.js
@@ -1,13 +1,13 @@
 const CategoriesData = [
   {
     id: '1',
-    categoryName: 'Breakfast',
+    categoryName: 'Creamy',
     cardColor: 'grey',
-    icon: 'https://static.vecteezy.com/system/resources/previews/022/660/505/non_2x/bread-with-berries-3d-breakfast-icon-png.png'
+    icon: 'https://pngimg.com/d/ice_cream_PNG20985.png'
   },
   {
     id: '2',
-    categoryName: 'Bread',
+    categoryName: 'Gelato',
     cardColor: 'green',
     icon: 'https://png.pngtree.com/png-clipart/20220918/original/pngtree-toast-icon-illustrasion-png-image_8622483.png'
   },
